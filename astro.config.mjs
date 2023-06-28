ffaac290b9315e52198e3bb0ae59d4fb1b921107
prannay <prannaykedia1@gmail.com>
2023-06-29 02:30:47 +0530

    Refactored Code

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,22 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/serverless";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    prefetch({
+      selector: "a[href^='/']",
+      throttle: 5,
+    }),
+    compress(),
+  ],
   output: "hybrid",
   adapter: vercel(),
 });
---
+++ src/data/resume.ts
@@ -0,0 +1,32 @@
+interface ResumePoint {
+  title: string;
+  time: string;
+  description: string;
+}
+
+export const resumePoints = <ResumePoint[]>[
+  {
+    title: "Chemical Engineering at Jadavpur University",
+    time: "2020 to 2024",
+    description:
+      "Pursuing my Bachelors in Chemical Engineering. Have participated in multiple activities throughout college, which includes leading JU's E-Cell, organising my department's 100 year anniversary celebration and facilitating the set-up of JU's own incubation centre.",
+  },
+  {
+    title: "Bombay Stock Exchange",
+    time: "May 2022 - July 2022",
+    description:
+      "Worked on analysing and generating insights from Mutual Fund Distribution data in Eastern India. Developed and implemented the MIS-Reporting system to improve productivity and boost conversion.",
+  },
+  {
+    title: "Bokaro Steel Plant",
+    time: "Dec 2022 - Dec 2022",
+    description:
+      " Completed intensive vocational training program at Bokaro Steel Plant, acquiring comprehensive knowledge and practical skills in steel production processes, machinery operation, and quality control measures.",
+  },
+  {
+    title: "Writee AI",
+    time: "Feb 2022 - Present",
+    description:
+      "Lead Writee AI as it's CEO & Co-founder. Actively worked on product development, legal compliances and finding product-market fit. Currently focused on obtaining scale through various marketing and development efforts.",
+  },
+];
