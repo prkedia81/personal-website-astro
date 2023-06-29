cd517a0cdd4380305da916d28a735334654b189c
prannay <prannaykedia1@gmail.com>
2023-06-29 14:06:27 +0530

    Added action to newsletter form

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,21 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
-
 import compress from "astro-compress";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(),
-    sitemap(),
-    prefetch({
-      selector: "a[href^='/']",
-      throttle: 5,
-    }),
-    compress(),
-  ],
+  integrations: [tailwind(), sitemap(), prefetch({
+    selector: "a[href^='/']",
+    throttle: 5
+  }), compress()],
   output: "hybrid",
-  adapter: vercel(),
-  site: "https://prannaykedia.com",
+  adapter: netlify(),
+  site: "https://prannaykedia.com"
 });
