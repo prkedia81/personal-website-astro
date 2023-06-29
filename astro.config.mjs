98e996414171e96023d10a7e3533efec7191dc12
prannay <prannaykedia1@gmail.com>
2023-06-29 14:17:36 +0530

    Added vercel config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,15 +5,22 @@ import sitemap from "@astrojs/sitemap";
 import prefetch from "@astrojs/prefetch";
 import compress from "astro-compress";
 
-import netlify from "@astrojs/netlify/functions";
+// import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), prefetch({
-    selector: "a[href^='/']",
-    throttle: 5
-  }), compress()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    prefetch({
+      selector: "a[href^='/']",
+      throttle: 5,
+    }),
+    compress(),
+  ],
   output: "hybrid",
-  adapter: netlify(),
-  site: "https://prannaykedia.com"
+  adapter: vercel({
+    imageService: true,
+  }),
+  site: "https://prannaykedia.com",
 });
