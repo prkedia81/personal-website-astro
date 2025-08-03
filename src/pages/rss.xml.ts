b90b4d2a6a63b43ce9f22c9fa672c7f99d51dd9d
Prannay Kedia <prannaykedia1@gmail.com>
2025-08-04 02:58:20 +0530

    Added rss feed

---
+++ src/pages/rss.xml.ts
@@ -0,0 +1,18 @@
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context: any) {
+  const blog = await getCollection("blogs");
+
+  return rss({
+    title: "Prannay Kedia's Blog",
+    description: "A blog about my experiences and learnings",
+    site: context.site,
+    items: blog.map((post) => ({
+      title: post.data.title,
+      pubDate: new Date(post.data.publishedDate ?? ""),
+      description: post.data.seoDescription,
+      link: `/blog/${post.data.slug}`,
+    })),
+  });
+}
